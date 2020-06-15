ec7e43dd48d6d0d0b026c601cf4a4c72b2c95856
Manuel Andaya <49893130+MDandaya@users.noreply.github.com>
2020-06-14 17:50:02 -0700

    Update database.js

--- util/database.js
+++ util/database.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 // connect to a database artistbook running on your localmachine
 const pool = mysql.createPool({
     host: 'remotemysql.com',
-    user: 'jPIhBvGiiy',
-    database: 'jPIhBvGiiy',
-    password: '7e1LWpSfbW'
+    user: 'b7UuofOqP2',
+    database: 'b7UuofOqP2',
+    password: ' 8LuOhBQah0'
 });
 
 module.exports = pool.promise();
